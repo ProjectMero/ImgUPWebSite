458a81300450f681fd99a6a8f927a6c24ee0dbb8
Rion <kanade.miyachi@gmail.com>
2025-09-27 18:17:21 +0900

    コメントアウト

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+/*
 document.addEventListener("DOMContentLoaded", () => {
     
     const header = document.querySelector("header");
@@ -15,3 +16,4 @@ document.addEventListener("DOMContentLoaded", () => {
             footer.innerHTML = data;
         });
 });
+*/
